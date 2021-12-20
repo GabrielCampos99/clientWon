e8254edb5f9a9692ada59f1fa682e54a3e7aa09f
Gabriel Campos <gabriel.campos@alicerceedu.com.br>
2021-12-20 16:07:07 -0300

    add button test

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('should render medium size button by default', () => {
+    renderWithTheme(<Button>Comprar</Button>)
+    expect(screen.getByRole('button', { name: /Comprar/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+  it('should render small size button when passed', () => {
+    renderWithTheme(<Button size="small">Comprar</Button>)
+    expect(screen.getByRole('button', { name: /Comprar/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render large size button when passed', () => {
+    renderWithTheme(<Button size="large">Comprar</Button>)
+    expect(screen.getByRole('button', { name: /Comprar/i })).toHaveStyle({
+      height: '5rem',
+      'font-size': '1.6rem',
+      padding: '0.8rem 4.8rem'
+    })
   })
 })
